cd758cf3da0120e070217f8015d7077eaa6a8e59
shahzod88888 <islomovdilbek01@gmail.com>
2025-04-09 22:36:35 +0500

    pythonlesson

---
+++ online 1-dars python/first lesson.py
@@ -0,0 +1,103 @@
+# print("Hello world" )  
+# print(type('salom'))
+# print(True + True)
+# print(type(True))
+
+# number: int = 123
+# text: str = 'salom'
+
+# number = 'salom'
+
+# print(number)
+
+
+# number = 10
+
+# print(number * number)
+
+# number:int = 10
+# name:str = 'Shahzod'
+
+# print("hello my name Shahzod")
+
+# a = 10
+# b = 30
+
+# # print(a ** b)
+# print(a ** (1/b))
+# print(b//a)
+
+
+# import math
+
+# # print(math.sqrt(24))
+# # print(math.isqrt(81))
+# # print(math.pow(2,4/2))
+# print(math.sin(360))
+# print(math.cos(2))
+# print(math.fabs(-29))
+
+# input('ismingizni kiriting')
+# print('salom \n dunyo')
+# print("""salom
+#       mening
+#       ismim
+#       Shahzod""")
+
+
+# ---------------------------------------------------------
+
+# name = 'shahzod'
+
+# print(f"salom  {name}")
+
+# age = 2005
+
+# print(f"salom mening yoshim {2024 - age} ga teng")
+
+# firstname = input('Ismingizni kiriting \n : ')
+# lastname = input('familyangizni kiriting \n : ')
+# age = input('Yoshingizni kiriting \n : ')
+
+# print(f"Slom {firstname} {lastname} sizning yoshingiz {age} da")
+
+# name = 'Sahzod'
+
+# print(len(name))
+
+
+# name = 'hasan'
+# # print(len(name))
+# name = name[:3]
+
+# name = 'Shahzod'
+
+# print(name.startswith('Shah'))
+# print(name.endswith('dod'))
+
+
+# print(name.lower())
+
+# name = 'Shahzod va'
+
+# print(name.upper())
+# print(name.capitalize())
+# print(name.title())
+
+# name = 'Shahzod Va Shahzod va Shahzod'
+
+# print(name.count('Shahzod'))
+
+# name = "shahzod va va va shahzod"
+
+# print(name.replace('va','yoq'))
+
+# def make_tags(tag, wors):
+
+#     return '<tag> </tag>'
+
+a = input("Kvadratning tomonini kiriting ")
+
+p = 4 * a
+
+print(f"Kvadrtning peremetri {p} ga teng ")
